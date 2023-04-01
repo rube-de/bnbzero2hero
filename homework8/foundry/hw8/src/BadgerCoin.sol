5602cfdb4dced77f4e6b9d3db23410c69e5c123d
beruf <rube@ruf2.de>
2023-04-01 14:17:11 +0200

    add contract for hw8

---
+++ homework8/foundry/hw8/src/BadgerCoin.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BadgerCoin is ERC20, Ownable {
+    constructor() ERC20("BadgerCoin", "BAC") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
