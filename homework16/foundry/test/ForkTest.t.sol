6934940582520c2fda5e763473826cd4c28a6cbb
beruf <rube@ruf2.de>
2023-04-30 18:26:49 +0200

    add pancake swap test

--- homework16/foundry/test/ForkTest.t.sol
+++ homework16/foundry/test/ForkTest.t.sol
@@ -3,20 +3,28 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
-import "./../src/IPeripheryImmutableState.sol";
+import "@pancakeswap/Interfaces/IPancakeRouter02.sol";
+import "@pancakeswap/Interfaces/IPancakeFactory.sol";
+import "@pancakeswap/Interfaces/IERC20.sol";
+import "@pancakeswap/Interfaces/IPancakePair.sol";
+
 
 contract ForkTest is Test {
   uint256 bnbFork;
   string BNB_RPC_URL = vm.envString("BNB_MAINNET_URL");
   address alice;
   address bob;
-  IPeripheryImmutableState pancakeSwapRouter;
+  IPancakeRouter02 pancakeSwapRouter;
+  IERC20 BUSD;
+  IERC20 WBNB;
 
   function setUp() public {
     bnbFork = vm.createFork(BNB_RPC_URL, 27802233);
     alice = address(1);
-    bob = address(0xffefE959d8bAEA028b1697ABfc4285028d6CEB10); // LEGO token holder
-    pancakeSwapRouter = IPeripheryImmutableState(0x13f4EA83D0bd40E75C8222255bc855a974568Dd4);
+    bob = address(0xF977814e90dA44bFA03b6295A0616a897441aceC); // WBNB token holder
+    pancakeSwapRouter = IPancakeRouter02(0x10ED43C718714eb63d5aA57B78B54704E256024E);
+    BUSD = IERC20(0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56);
+    WBNB = IERC20(0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c);
   }
 
   function testChain() public {
@@ -31,6 +39,34 @@ contract ForkTest is Test {
   function testPancakeSwap() public {
     vm.selectFork(bnbFork);
     console.log(pancakeSwapRouter.factory());
-    assertEq(pancakeSwapRouter.factory(), address(0x0BFbCF9fa4f9C56B0F40a671Ad40E0805A091865));
+    assertEq(pancakeSwapRouter.factory(), address(0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73));
+  }
+
+  function testPairStats() public {
+    vm.selectFork(bnbFork);
+    IPancakeFactory factory = IPancakeFactory(pancakeSwapRouter.factory());
+    IPancakePair pair = IPancakePair(factory.getPair(address(BUSD), address(WBNB)));
+    assertEq(address(pair), address(0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16));
+    (uint112 r0, uint112 r1, uint32 time) = pair.getReserves();
+    console.log("r0 = %s, r1 = %s, time = %s ", r0, r1, time);
+  }
+
+  function testSwap() public {
+    vm.selectFork(bnbFork);
+    vm.prank(bob);
+    WBNB.approve(address(pancakeSwapRouter), 1e18);
+    address[] memory path = new address[](2);
+    path[0] = address(WBNB);
+    path[1] = address(BUSD);
+    vm.prank(bob);
+    pancakeSwapRouter.swapExactTokensForTokens(
+      1e18,
+      2e10,
+      path,
+      address(bob),
+      block.timestamp+11
+    );
+
+
   }
 }
